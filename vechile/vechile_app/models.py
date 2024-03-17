5afdff5fbdfecfa2a54b2a3e05de77c20a8dee26
Habtamu210 <beleyaha@gmail.com>
2024-03-17 02:38:13 -0500

    Create Vechile models

--- vechile/vechile_app/models.py
+++ vechile/vechile_app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Vechile(models.Model):
+    plate_number = models.CharField(max_length=15, primary_key=True)
+    type = models.CharField(max_length=50)
+    capacity = models.IntegerField()
